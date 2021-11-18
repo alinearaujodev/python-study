40f4c887d0091d84a6b1342801b1f11ed2f78903
alinearaujodev <alinearaujodev@gmail.com>
2021-11-18 20:22:23 -0300

    explicação do uso do int

--- 02-exercicios/01-simples/03-soma.py
+++ 02-exercicios/01-simples/03-soma.py
@@ -1,6 +1,6 @@
 # leia dois numeros e soma eles
 
-num1 = int(input('Number 1: '))
+num1 = int(input('Number 1: ')) # para que o valor do input seja como num, será precisar colocar o int
 num2 = int(input ('Number 2: '))
 
 print('Soma:', num1 + num2)
