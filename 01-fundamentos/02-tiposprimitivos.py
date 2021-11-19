624a162cdbaa21f798b5239761bf3c3631d99bc4
alinearaujodev <alinearaujodev@gmail.com>
2021-11-18 21:08:28 -0300

    teoria dos tipos primitivos

---
+++ 01-fundamentos/02-tiposprimitivos.py
@@ -0,0 +1,24 @@
+# int
+a = 1
+b = -4
+c = 0
+d = 8942
+
+# float
+e = 4.5
+f = 0.543
+g = -12.543
+h = 8942.0
+
+# bool
+i = True
+j = False
+
+# str
+k = 'Olá mundo!'
+l = '7.4'
+m = ''
+
+# prints
+print('O valor de A:', a)
+print('O valor de A {}'.format(a)) # tudo que estiver dentro de .format será substituido nas chaves
