b2dd41ddde3a38be63f38cf681e7433fdbe7a1df
alinearaujodev <alinearaujodev@gmail.com>
2021-11-24 16:48:10 -0300

    add como comentar no codigo em bloco

--- 01-fundamentos/01-helloworld.py
+++ 01-fundamentos/01-helloworld.py
@@ -2,8 +2,11 @@
 print('hello world') 
 
 # variavéis e lendo dados do usuário com função input
-
 nome = input('Qual seu nome: ')
 idade = input('Qual sua idade: ')
 peso = input('Qual seu peso: ')
 print(nome, idade, peso)
+
+# COMO EU COMENTO NO CÓDIGO
+print('USE O # PARA COMENTARIOS EM LINHA')
+print('USE O \"\"\" \"\"\" PARA COMENTARIOS EM BLOCO')
