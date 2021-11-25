ad530907a314b63b3ba8e4e0a64905d13d14b909
alinearaujodev <alinearaujodev@gmail.com>
2021-11-25 16:06:17 -0300

    created file 03-numbers e include functions math

---
+++ 01-fundamentos/03-numbers.py
@@ -0,0 +1,25 @@
+from math import * # importando a biblioteca math do python 
+
+print(1 + 1) # soma
+print(5 - 2) # subtração
+print(7 * 4) # multiplicação
+print(9 / 6) # divisão 
+print(10 % 3) # resto da divisão
+
+"""
+Vai funcionar somente com a concatenação com o + 
+porque o number ficou como uma string
+Agora de tirar a função str, vai dar typeerror 
+
+"""
+my_num = 15
+print(str(my_num) + ' my favorite number') 
+
+#  FUNÇÕES NICE
+print(pow(9,2)) # potência
+print(max(100, 73)) # retorna o maior numero 
+print(min(7, 28)) # retorna o menor numero
+print(round(23.67)) # arrendonda para mais ou para menos 
+print(floor(3.8)) # arrendonda para menos 
+print(ceil(3.8)) # arrendonda para mais
+print(sqrt(81)) # raiz quadrada 
