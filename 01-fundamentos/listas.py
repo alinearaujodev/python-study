59b771fc35c447bc0494adb7fde2e8a7242eaf62
alinearaujodev <alinearaujodev@gmail.com>
2021-12-23 15:20:12 -0300

    estudado listas

---
+++ 01-fundamentos/listas.py
@@ -0,0 +1,13 @@
+familia = ["aline", "heloísa", "joyce", "fábio", "adriana"]
+aniversario = [15, 1, 26, 22, 10]
+
+# FUNÇÕES PARA LISTAS
+familia.extend(aniversario) # JUNTAR LISTAS
+familia.append("Allisson") # adicionar um elemento da lista
+familia.insert(3, "Luis") # add elemento na posição escolhida
+familia.remove("Allisson") # removendo um elemento da lista
+aniversario.sort() # lista em ordem (numerica ou alfabetica)
+aniversario.reverse() # colocando a lista inverso
+
+print(familia)
+print(aniversario)
