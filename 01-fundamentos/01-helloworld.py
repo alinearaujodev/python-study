af31d8fd2d3fdaa39d904b48a46873f87d22fe77
alinearaujodev <alinearaujodev@gmail.com>
2022-02-21 15:42:19 -0300

    comentários

--- 01-fundamentos/01-helloworld.py
+++ 01-fundamentos/01-helloworld.py
@@ -7,6 +7,6 @@ idade = input('Qual sua idade: ')
 peso = input('Qual seu peso: ')
 print(nome, idade, peso)
 
-# COMO EU COMENTO NO CÓDIGO
-print('USE O # PARA COMENTARIOS EM LINHA')
+# COMENTÁRIOS NO CÓDIGO
+print('\nUSE O # PARA COMENTARIOS EM LINHA')
 print('USE O \"\"\" \"\"\" PARA COMENTARIOS EM BLOCO')
