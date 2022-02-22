9119fceead3083225240fb5719eb0b0b31f575d6
alinearaujodev <alinearaujodev@gmail.com>
2022-02-22 11:32:43 -0300

    explicação sobre listas

--- 01-fundamentos/05-listas.py
+++ 01-fundamentos/05-listas.py
@@ -1,9 +1,11 @@
-# listas são estruturas de dados
-# caso adicione um valor, poder ser alterado depois
+# listas são estruturas de dados, pode ser escrita como uma lista de valores (itens) separados por vírgula, entre colchetes.
+# listas são mutáveis, caso adicione um valor, poder ser alterado depois. 
+# além que podem ser de diferente tipos
 
 
 familia = ["aline", "heloísa", "joyce", "fábio", "adriana"]
 aniversario = [15, 1, 26, 22, 10]
+pessoa = ["aline", 15, True]
 
 # FUNÇÕES PARA LISTAS
 familia.extend(aniversario) # JUNTAR LISTAS
@@ -12,6 +14,9 @@ familia.insert(3, "Luis") # add elemento na posição escolhida
 familia.remove("Allisson") # removendo um elemento da lista
 aniversario.sort() # lista em ordem (numerica ou alfabetica)
 aniversario.reverse() # colocando a lista inverso
+x = [pessoa, aniversario]
 
 print(familia)
 print(aniversario)
+print(x)
+print(len(pessoa))
