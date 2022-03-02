0a7ad6b2e147e2b348936313dbe71849c70b9e30
alinearaujodev <alinearaujodev@gmail.com>
2022-03-02 17:53:51 -0300

    formatado a docstring

--- 02-exercicios/01-simples/01-boavindas.py
+++ 02-exercicios/01-simples/01-boavindas.py
@@ -1,4 +1,8 @@
-# Leia o nome de uma pessoa e mostre uma mensagem de boas-vindas com o valor digitado
+"""
+Boas-Vindas!
+
+Leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+"""
 
 name = input('Olá, qual seu nome: ')
 print('Seja bem vindo (a) ' + name)
